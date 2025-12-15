e624ae0587c90a392a472544a3f1ae0a66c9ffd6
Claes <18647474+GlitterDelight@users.noreply.github.com>
2025-12-15 17:56:52 +0100

    done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let output = ""
+
+    for (let i = 0; i < string.length; i++) {
+        output = string.at(i) + output;
+    }
+
+    return output
 };
 
 // Do not edit below this line
