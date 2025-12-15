94a154db2ad5c3fb43faeeee6d39850f542aa778
Claes <18647474+GlitterDelight@users.noreply.github.com>
2025-12-15 18:24:31 +0100

    done with filter method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    return arr.filter(function(arrItem) {
+        if (!args.includes(arrItem))
+            return true
+    })
 };
 
 // Do not edit below this line
