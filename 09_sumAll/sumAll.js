4de4271379225a8c088177922fa7fd7408e342b8
Claes <18647474+GlitterDelight@users.noreply.github.com>
2025-12-23 07:48:48 +0100

    1-7 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    let sum = 0
+    let min = 0
+    let max = 0
 
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo))
+        return "ERROR"
+
+    if (numberOne < 0 || numberTwo < 0)
+        return "ERROR"
+
+    if (numberOne > numberTwo) {
+        max = numberOne
+        min = numberTwo
+    } else if (numberOne < numberTwo) {
+        max = numberTwo
+        min = numberOne
+    } else {
+        return numberOne
+    }
+
+        for (let i = min; i <= max; i++)
+            sum += i
+
+    return sum;
 };
 
 // Do not edit below this line
